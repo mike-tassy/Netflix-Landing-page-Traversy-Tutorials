31a4eeff2b808374a8a9eda973b13b5552163190
Mike <tassyamudamichael@gmail.com>
2025-05-08 11:25:28 +0100

    Completed the javascript and updated the html and css

---
+++ node.js
@@ -0,0 +1,33 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+
+// This will select tab content item
+function selectItem(e) {
+
+// Remove border from all tabs
+removeBorder();
+removeshow();
+//Add border to current tab
+this.classList.add('tab-border');
+
+// Grab content from the DOM
+const tabContentItem = document.querySelector(`#${this.id}-content`);
+// Add show class
+tabContentItem.classList.add('show');
+
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeshow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+
+console.log(this.id);
